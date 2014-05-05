d76d5c65c1957926332cbfab1915fb6d1d4c2e54
manik <manik@couchbase.com>
2014-05-06 01:19:56 +0530

    Add example application

---
+++ example/example_client/example_client.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/couchbaselabs/retriever/logger"
+	"github.com/couchbaselabs/retriever/stats"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+var lw *logger.LogWriter
+var sc *stats.StatsCollector
+
+const EC = "ExampleClient"
+
+const (
+	RESPONSE_OK = iota
+	RESPONSE_INVALID_CMD
+	RESPONSE_INVALID_MSG
+	RESPONSE_UNKNOWN_ERROR
+)
+
+const (
+	CMD_HELLO = iota
+	CMD_STATS
+	CMD_DATA
+	CMD_RESTART
+)
+
+type Command struct {
+	TransactionId int
+	Cmd           int
+	Message       string
+}
+
+type Response struct {
+	TransactionId int
+	ResponseCode  int
+	Message       string
+}
+
+const (
+	STAT_REQUESTS      = "requests"
+	STAT_FAILURES      = "failures"
+	STAT_BYTESTRANS    = "bytesSent"
+	STAT_BYTESRECEIVED = "bytesReceived"
+)
+
+func do_requests(clientId int, uri string) {
+
+	command := Command{}
+	response := Response{}
+	i := 0
+	client := &http.Client{}
+	for {
+
+		i++
+		sc.IncrementStat(STAT_REQUESTS)
+		command.TransactionId = (clientId * 1000000) + i
+		command.Cmd = i % 4
+		command.Message = "Client Id " + string(clientId)
+		reqBody, err := json.Marshal(command)
+		transactionId := fmt.Sprintf("%d", command.TransactionId)
+		if err != nil {
+			lw.LogError(transactionId, EC, "Error marshalling %s", err.Error())
+		}
+		lw.LogDebug(transactionId, EC, "Sending command %d", command.Cmd)
+
+		r, err := http.NewRequest("POST", uri, bytes.NewBufferString(string(reqBody)))
+		resp, err := client.Do(r)
+		if err != nil {
+			lw.LogError(transactionId, EC, "Error sending HTTP request %s", err.Error())
+			sc.IncrementStat(STAT_FAILURES)
+			continue
+		}
+
+		sc.UpdateStat(STAT_BYTESTRANS, sc.GetStat(STAT_BYTESTRANS).(int)+len(reqBody))
+
+		respBody, _ := ioutil.ReadAll(resp.Body)
+		if err := json.Unmarshal(respBody, &response); err != nil {
+			lw.LogError(transactionId, EC, "Cannot read response %s", err.Error())
+			sc.IncrementStat(STAT_FAILURES)
+			continue
+		}
+		lw.LogDebug(transactionId, "EC", "Received response from server %s", response.Message)
+		sc.UpdateStat(STAT_BYTESRECEIVED, sc.GetStat(STAT_BYTESTRANS).(int)+len(respBody))
+		time.Sleep(300 * time.Millisecond)
+	}
+}
+
+func main() {
+	var err error
+	lw, err = logger.NewLogger("ExampleClient", logger.LevelInfo)
+	if err != nil {
+		fmt.Printf("Cannot create logger instance %s", err.Error())
+		panic("die")
+	}
+	lw.EnableKeys([]string{EC})
+	lw.SetFile("ExampleClient.log")
+	sc, err = stats.NewStatsCollector(EC)
+	if err != nil {
+		lw.LogError("", EC, "Unable to initialize stats module %s", err.Error())
+	}
+
+	sc.AddStatKey(STAT_REQUESTS, 0)
+	sc.AddStatKey(STAT_FAILURES, 0)
+	sc.AddStatKey(STAT_BYTESTRANS, 0)
+	sc.AddStatKey(STAT_BYTESRECEIVED, 0)
+	do_requests(1, "http://localhost:9191/command/")
+}
---
+++ example/example_server/example_server.go
@@ -0,0 +1,138 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/couchbaselabs/retriever/logger"
+	"github.com/couchbaselabs/retriever/stats"
+	"math/rand"
+	"net/http"
+)
+
+var lw *logger.LogWriter
+var sc *stats.StatsCollector
+
+const ES = "ExampleServer"
+
+const (
+	RESPONSE_OK = iota
+	RESPONSE_INVALID_CMD
+	RESPONSE_INVALID_MSG
+	RESPONSE_UNKNOWN_ERROR
+)
+
+const (
+	CMD_HELLO = iota
+	CMD_STATS
+	CMD_DATA
+	CMD_RESTART
+)
+
+type Command struct {
+	TransactionId int
+	Cmd           int
+	Message       string
+}
+
+type Response struct {
+	TransactionId int
+	ResponseCode  int
+	Message       string
+}
+
+var answers = []string{
+	"It is certain",
+	"It is decidedly so",
+	"Without a doubt",
+	"Yes definitely",
+	"You may rely on it",
+	"As I see it yes",
+	"Most likely",
+	"Outlook good",
+	"Yes",
+	"Signs point to yes",
+	"Reply hazy try again",
+	"Ask again later",
+	"Better not tell you now",
+	"Cannot predict now",
+	"Concentrate and ask again",
+	"Don't count on it",
+	"My reply is no",
+	"My sources say no",
+	"Outlook not so good",
+	"Very doubtful",
+}
+
+func cmdHandler(w http.ResponseWriter, r *http.Request) {
+	command := Command{}
+	response := Response{}
+
+	sc.IncrementStat("Requests")
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&command)
+	if err != nil {
+		lw.LogError("", ES, "Unable to decode message from client")
+		sc.IncrementStat("Failures")
+		http.Error(w, "Unable to decode message", http.StatusInternalServerError)
+	}
+
+	transactionId := fmt.Sprintf("%d", command.TransactionId)
+	bytesRecvd := sc.GetStat("bytesReceived").(int)
+	sc.UpdateStat("bytesReceived", bytesRecvd+int(r.ContentLength))
+	lw.LogDebug(transactionId, ES, "Received command %s message %s", command.Cmd, command.Message)
+
+	response.TransactionId = command.TransactionId
+	response.ResponseCode = RESPONSE_OK
+
+	switch command.Cmd {
+	case CMD_HELLO:
+		response.Message = "AOK"
+	case CMD_STATS:
+		response.Message = sc.GetAllStat()
+		lw.LogDebug(transactionId, ES, "Stats request received")
+	case CMD_DATA:
+		response.Message = answers[rand.Intn(len(answers))]
+	case CMD_RESTART:
+		response.Message = "Sorry, No can do "
+		lw.LogWarn(transactionId, ES, "Unable to restart at this point")
+	default:
+		sc.IncrementStat("Failures")
+		response.ResponseCode = RESPONSE_INVALID_CMD
+		lw.LogError(transactionId, ES, "Invalid command code %d", command.Cmd)
+	}
+
+	lw.LogDebug(transactionId, ES, "Response message %s message %s", response.ResponseCode, response.ResponseCode)
+
+	respBody, err := json.Marshal(response)
+	sc.UpdateStat("bytesSent", sc.GetStat("bytesSent").(int)+len(respBody))
+	lw.LogDebug("", ES, "Bytes sent %d", sc.GetStat("bytesSent"))
+
+	fmt.Fprintf(w, string(respBody))
+
+}
+
+func main() {
+	http.HandleFunc("/command/", cmdHandler)
+	var err error
+	lw, err = logger.NewLogger("ExampleServer", logger.LevelInfo)
+	if err != nil {
+		fmt.Printf("Cannot create logger instance %s", err.Error())
+		panic("die")
+	}
+	lw.EnableKeys([]string{ES})
+	lw.SetFile("ExampleServer.log")
+	sc, err = stats.NewStatsCollector(ES)
+	if err != nil {
+		lw.LogError("", ES, "Unable to initialize stats module %s", err.Error())
+	}
+
+	sc.AddStatKey("Requests", 0)
+	sc.AddStatKey("Success", 0)
+	sc.AddStatKey("Failures", 0)
+	sc.AddStatKey("Server Port", 9191)
+	sc.AddStatKey("bytesReceived", 0)
+	sc.AddStatKey("bytesSent", 0)
+
+	lw.LogInfo("", ES, "Example Server starting on port 9191")
+	http.ListenAndServe(":9191", nil)
+}
